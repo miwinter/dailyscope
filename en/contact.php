43cc1d1c7460c57f700bb4ed2423d93767ed9a4c
Michel Winter <winter.michel@gmail.com>
2025-11-26 06:25:28 +0100

    Build for 2025-11-26

--- en/contact.php
+++ en/contact.php
@@ -22,6 +22,27 @@ function render_page($title, $inner_html) {
 <link href="/assets/css/style.css" rel="stylesheet">
 <link rel="stylesheet" href="/assets/css/owl.carousel.min.css">
 <link rel="stylesheet" href="/assets/css/owl.theme.default.min.css">
+
+<script>
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        page_lang: 'en',
+        page_type: 'contact_processing',
+        
+        
+        
+});
+    (function(w,d,s,l,i){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),
+            dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KL7V4FQH');
+</script>
 EOT;
 
   $header = <<<'EOT'
@@ -65,13 +86,13 @@ EOT;
               <a class="nav-link active" aria-current="page" href="/">News</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/en/2025-11-25/country_counts.html">Top Countries</a>
+              <a class="nav-link active" aria-current="page" href="/en/2025-11-26/country_counts.html">Top Countries</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/en/2025-11-25/labels.html">Top Themes</a>
+              <a class="nav-link active" aria-current="page" href="/en/2025-11-26/labels.html">Top Themes</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/en/2025-11-25/c1.html">In Focus</a>
+              <a class="nav-link active" aria-current="page" href="/en/2025-11-26/c1.html">In Focus</a>
             </li>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="/en/about.html">About</a>
@@ -117,13 +138,13 @@ EOT;
                <a href="/">News </a>
             </li>
             <li>
-              <a href="/en/2025-11-25/country_counts.html">Top Countries</a>
+              <a href="/en/2025-11-26/country_counts.html">Top Countries</a>
             </li>
             <li>
-              <a href="/en/2025-11-25/labels.html">Top Themes</a>
+              <a href="/en/2025-11-26/labels.html">Top Themes</a>
             </li>
             <li>
-              <a href="/en/2025-11-25/c1.html">In Focus</a>
+              <a href="/en/2025-11-26/c1.html">In Focus</a>
             </li>
             <li>
               <a href="/en/about.html">About</a>
