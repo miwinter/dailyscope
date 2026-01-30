1fcd0b6b8b90dc95f8a45e6428fb0b9779b18a89
Michel Winter <winter.michel@gmail.com>
2026-01-30 06:27:59 +0100

    Build for 2026-01-30

--- fr/contact.php
+++ fr/contact.php
@@ -95,13 +95,13 @@ EOT;
               <a class="nav-link active" aria-current="page" href="/fr/">Actus</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/fr/2026-01-29/country_counts.html">Pays</a>
+              <a class="nav-link active" aria-current="page" href="/fr/2026-01-30/country_counts.html">Pays</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/fr/2026-01-29/labels.html">Thèmes</a>
+              <a class="nav-link active" aria-current="page" href="/fr/2026-01-30/labels.html">Thèmes</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/fr/2026-01-29/c1.html">France Focus</a>
+              <a class="nav-link active" aria-current="page" href="/fr/2026-01-30/c1.html">France Focus</a>
             </li>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="/fr/about.html">A Propos</a>
@@ -150,13 +150,13 @@ EOT;
                <a href="/">Actus </a>
             </li>
             <li>
-              <a href="/fr/2026-01-29/country_counts.html">Pays</a>
+              <a href="/fr/2026-01-30/country_counts.html">Pays</a>
             </li>
             <li>
-              <a href="/fr/2026-01-29/labels.html">Thèmes</a>
+              <a href="/fr/2026-01-30/labels.html">Thèmes</a>
             </li>
             <li>
-              <a href="/fr/2026-01-29/c1.html">France Focus</a>
+              <a href="/fr/2026-01-30/c1.html">France Focus</a>
             </li>
             <li>
               <a href="/fr/about.html">A Propos</a>
