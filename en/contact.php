5cad5e88408c87b87a77524ba33ea6a58e2a07e2
Michel Winter <winter.michel@gmail.com>
2026-03-01 06:26:05 +0100

    Build for 2026-03-01

--- en/contact.php
+++ en/contact.php
@@ -95,13 +95,13 @@ EOT;
               <a class="nav-link active" aria-current="page" href="/">News</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/en/2026-02-28/country_counts.html">Top Countries</a>
+              <a class="nav-link active" aria-current="page" href="/en/2026-03-01/country_counts.html">Top Countries</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/en/2026-02-28/labels.html">Top Themes</a>
+              <a class="nav-link active" aria-current="page" href="/en/2026-03-01/labels.html">Top Themes</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/en/2026-02-28/c1.html">In Focus</a>
+              <a class="nav-link active" aria-current="page" href="/en/2026-03-01/c1.html">In Focus</a>
             </li>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="/en/about.html">About</a>
@@ -150,13 +150,13 @@ EOT;
                <a href="/">News </a>
             </li>
             <li>
-              <a href="/en/2026-02-28/country_counts.html">Top Countries</a>
+              <a href="/en/2026-03-01/country_counts.html">Top Countries</a>
             </li>
             <li>
-              <a href="/en/2026-02-28/labels.html">Top Themes</a>
+              <a href="/en/2026-03-01/labels.html">Top Themes</a>
             </li>
             <li>
-              <a href="/en/2026-02-28/c1.html">In Focus</a>
+              <a href="/en/2026-03-01/c1.html">In Focus</a>
             </li>
             <li>
               <a href="/en/about.html">About</a>
